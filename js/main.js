089f3ce730911c3cd7cde2efc66ad338717e0466
Idedav <xdavide.correa@gmail.com>
2023-09-25 14:48:12 +0200

    [add] dinamic posts

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ const posts = [
         media: "https://unsplash.it/600/400?image=24",
         author: {
             name: "Luca Formicola",
-            image: null
+            image: "https://unsplash.it/300/300?image=40"
         },
         likes: 56,
         created: "2021-04-03"
@@ -55,3 +55,42 @@ const posts = [
         created: "2021-03-05"
     }
 ];
+
+const containerPost = document.getElementById('container');
+
+
+posts.forEach(post => {
+    const {author, created, content, media, likes} = post;
+    containerPost.innerHTML +=`
+                <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${author.image}" alt="Phil Mangione">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${author.name}</div>
+                        <div class="post-meta__time">${created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${content}</div>
+            <div class="post__image">
+                <img src="${media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <button class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </button>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    `
+});
